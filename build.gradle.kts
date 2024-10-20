aafdeb57d72b416a328052c124da66f9bd0737b1
AsmaaEltalawy <146077807+AsmaaEltalawy@users.noreply.github.com>
2024-10-20 23:30:11 +0300

    Save local changes before merge

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
-    id ("androidx.navigation.safeargs") version "2.7.3" apply false
+    id ("androidx.navigation.safeargs") version "2.8.3" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
     id("com.google.devtools.ksp") version "1.8.10-1.0.9" apply false
     id("com.google.dagger.hilt.android") version "2.51.1" apply false
